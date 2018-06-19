20c714ead245d3c3cd3d7b30a8e6bb1154e8f373
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-06-19 16:08:56 -0300

    Implement Credo suggestions

--- apps/database/test/test_helper.exs
+++ apps/database/test/test_helper.exs
@@ -1,7 +1,7 @@
 ExUnit.start()
 
 DataBase.Repos.AmethystRepo
-|>Ecto.Adapters.SQL.Sandbox.mode(:manual)
+|> Ecto.Adapters.SQL.Sandbox.mode(:manual)
 
 Code.load_file("test/support/faker.exs")
 Code.load_file("test/support/factories/accounts.exs")
